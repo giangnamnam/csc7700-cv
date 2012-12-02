5d539c2512c2e2d94554ca956254e80d359076d7
der.kleine.mauserich@gmail.com <der.kleine.mauserich@gmail.com@710100cc-46eb-0444-1e5a-48f847c5704d>
2012-12-02 06:37:39 +0000

    The integral image detector. See comments for how it works.

--- sem-project/OMS/IntegralImageStopSignDetector.cs
+++ sem-project/OMS/IntegralImageStopSignDetector.cs
@@ -19,10 +19,65 @@ namespace OMS.CVApp.SignDetector
         {
         }
 
-        public override Rectangle[] find(Image<Bgr, Byte> image)
+        public override Rectangle find(Image<Bgr, Byte> orig)
         {
-            Console.WriteLine("Unimplemented: find() in IntegralImageStopSignDetector.");
-            return null;
+          //Image<Bgr, Byte> orig = new Image<Bgr, Byte>("stop-sign-model.png");
+
+            // Make square matrix.
+            int S;
+            if (orig.Rows > orig.Cols) {
+                S = orig.Rows;
+            } else {
+                S = orig.Cols;
+            }
+            Image<Bgr, Byte> img  = orig.Resize(S, S, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR);
+
+            int[,] I = new int[img.Rows,img.Cols];
+            int[]  D = new int[img.Cols];
+            int i,j,a=0,b=0,c=0,d=0;
+            
+            // Compute integral image.
+            for (i=0; i<img.Rows; i++) {
+                for (j=0; j<img.Cols; j++) {
+                    I[i,j] = img.Data[i,j,2] + I[i-1,j] + I[i,j-1]- I[i-1,j-1];
+                }
+            }
+            
+            // Take differences, find maxima a and b.
+            D[0] = 0;
+            for (i = 1; i < img.Rows; i++) {
+                D[i] = I[i, i] - I[i - 1, i - 1];
+                if (D[i] > a) {
+                    a = i;
+                }
+                if (D[i] > b && D[i] < a) {
+                    b = i;
+                }
+            }
+
+            // Find inner maxima c and d.
+            for (i = a; i < b; i++)
+            {
+                if (D[i] > c)
+                {
+                    c = i;
+                }
+                if (D[i] > c && D[i] < d)
+                {
+                    d = i;
+                }
+            }
+
+            float s_y = orig.Rows/img.Rows;
+            float s_x = orig.Cols/img.Cols;
+
+            int x = (int)(c*s_x);
+            int y = (int)(c*s_y);
+            int w = (int)((d-c)*s_x);
+            int h = (int)((d-c)*s_y);
+
+            Rectangle R = new Rectangle(x,y,w,h);
+            return R;
         }
 
         public override Image<Bgr, Byte> annotate(Image<Bgr, Byte> image)
@@ -30,5 +85,7 @@ namespace OMS.CVApp.SignDetector
             Console.WriteLine("Unimplemented: annotate() in IntegralImageStopSignDetector.");
             return null;
         }
+
+        public object Rectangle { get; set; }
     }
 }
